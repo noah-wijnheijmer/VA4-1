2901496c0b28665755909726a67575987764ef1b
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-13 22:34:39 +0100

    Update VA4-1.py

--- VA4-1.py
+++ VA4-1.py
@@ -16,7 +16,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-#import geopandas as gpd
+import geopandas as gpd
 import plotly.express as px
 import streamlit as st
 
