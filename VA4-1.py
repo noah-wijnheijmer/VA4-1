ed66e5d5fc684f9be0ff729e846f9e2032c7ed67
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-13 22:20:53 +0100

    Update VA4-1.py

--- VA4-1.py
+++ VA4-1.py
@@ -308,7 +308,7 @@ elif pages == 'Visualisaties':
     st.markdown("Hieronder wordt een grafiek weergegeven met data wereldwijd over de unemployment rate.")
     st.plotly_chart(fig, use_container_width=True)
     st.markdown("Hieronder wordt een barplot weergegeven met een overzicht van de 10 hoogste unemployment rate landen wereldwijd.")
-    st.pyplot(fig1, use_container_width=True) 
+    st.pyplot(fig1) 
     #st.markdown("Hieronder wordt een grafiek weergegeven met een overzicht van de 10 laagste unemployment rate landen wereldwijd.")
     #st.pyplot(fig3, use_container_width=True)
 elif pages == 'Einde':
