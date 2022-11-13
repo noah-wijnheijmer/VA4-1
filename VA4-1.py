2842c7904a7c834d59c1c69b78877663024f6acf
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-13 20:52:19 +0100

    Update VA4-1.py

--- VA4-1.py
+++ VA4-1.py
@@ -304,7 +304,7 @@ elif pages == 'Datasets':
     st.markdown("Deze dataset gebruiken wij om te vergelijken op land code, wat de correlatie is tussen de unemployment rate en de happiness rate in een land. deze data sets zijn samengevoegd door de 'Country Name', te mergen.")
     st.dataframe(data=happiness_merge, use_container_width=False)
 elif pages == 'Visualisaties':
-    st.subheader("Hier worden de visualisaties weergegeven die wij hebben opgesteld."), st.line_chart(fig) 
+    st.subheader("Hier worden de visualisaties weergegeven die wij hebben opgesteld.") #st.line_chart(fig) 
 elif pages == 'Einde':
     st.markdown('Bedankt voor het bezoeken.')
     st.markdown('Noah Wijnheimer, Julius Slobbe')
