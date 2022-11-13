7f8c8ce9ca01c7a9934bae0e81d5699c7a601b70
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-13 22:48:42 +0100

    Update VA4-1.py

--- VA4-1.py
+++ VA4-1.py
@@ -167,7 +167,7 @@ countries = gpd.read_file('countries.geojson')
 dfn = countries.merge(unemp_long, left_on='ISO_A3', right_on='Country Code',how='right')
 
 m = folium.Map(location=[0,0], zoom_start=1, zoom_control=False, tiles='Cartodb Positron')
-    folium.Choropleth(geo_data = countries,                 
+folium.Choropleth(geo_data = countries,                 
     name = 'geometry',                 
     data = dfn,               
     columns = ['ADMIN', 'Unemployment Rate'],              
