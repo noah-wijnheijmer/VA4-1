c4e8bcea045428db39fdde5731837d0f1fa2b7ab
SCNnuts <114662407+SCNnuts@users.noreply.github.com>
2022-11-14 11:42:18 +0100

    Update VA4.py

--- VA4.py
+++ VA4.py
@@ -299,7 +299,7 @@ st.sidebar.title('Navigatie')
 # In[41]:
 
 
-pages = st.sidebar.radio('paginas', options=['Home','Datasets', 'Visualisaties', 'Einde'])
+pages = st.sidebar.radio('paginas', options=['Home','Datasets', 'Visualisaties', 'Wereld Kaart', 'Einde'])
 
 if pages == 'Home':
     st.markdown("Welkom op het dashboard van Noah Wijnheijmer en Julius Slobbe. Samen hebben wij gekozen om de dataset 'unemployment analysis' te kiezen, dit hebben wij gedaan omdat het ten eerste ons een leuke dataset leek en ten tweede omdat wij vonden dat er goed gewerkt kon worden met deze data set.")
@@ -318,7 +318,9 @@ elif pages == 'Visualisaties':
     st.markdown("Hieronder wordt een barplot weergegeven met een overzicht van de 10 hoogste unemployment rate landen wereldwijd.")
     st.pyplot(fig1) 
     st.markdown("Hieronder wordt een grafiek weergegeven met een overzicht van de 10 laagste unemployment rate landen wereldwijd.")
-    st.pyplot(fig3), folium_static(m)
+    st.pyplot(fig3)
+elif pages == 'Wereld Kaart':    
+    folium_static(m)
 elif pages == 'Einde':
     st.markdown('Bedankt voor het bezoeken.')
     st.markdown('Noah Wijnheimer, Julius Slobbe')
